aa863081d4a091408a8ff12c22944e89ef8176d4
paw <pawrequest@users.noreply.github.com>
2024-02-27 12:12:24 +0000

    imports resolved detatched?

--- src/pycommence/__init__.py
+++ src/pycommence/__init__.py
@@ -1,4 +1,5 @@
-from . import api
+from .api.cmc_csr import Csr, get_csr, csr_context
+from .api.cmc_db import Cmc
+from .api.cmc_types import CmcFilter, FilterArray, FilterCondition, FilterTypeEnum, Connection
 
-
-__all__ = ['api']
+__all__ = ['Cmc', 'Csr', 'CmcFilter', 'FilterArray', 'get_csr', 'csr_context', 'FilterCondition', 'FilterTypeEnum', 'Connection']
--- src/pycommence/api/__init__.py
+++ src/pycommence/api/__init__.py
@@ -1,8 +1,8 @@
-from __future__ import annotations
-
-from .cmc_csr import Csr, get_csr, csr_context
-from .cmc_db import Cmc
-from .cmc_types import CmcFilter, FilterArray
-
-__all__ = ['Cmc', 'Csr', 'CmcFilter', 'FilterArray', 'get_csr', 'csr_context']
+# from __future__ import annotations
+#
+# from .cmc_csr import Csr, get_csr, csr_context
+# from .cmc_db import Cmc
+# from .cmc_types import CmcFilter, FilterArray, FilterCondition, FilterTypeEnum
+#
+# __all__ = ['Cmc', 'Csr', 'CmcFilter', 'FilterArray', 'get_csr', 'csr_context', 'FilterCondition', 'FilterTypeEnum']
 
