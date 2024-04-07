bf1f3bdf175bb5b4126c8a3286519893c25bb4ff
paw <pawrequest@users.noreply.github.com>
2024-04-07 18:06:02 +0100

    tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,30 +1,52 @@
-import random
-
-import pytest
-
-from pycommence import api
-import sample_data
 
 
-@pytest.fixture
-def hire_csr():
-    with api.csr_context("Hire") as csr:
-        yield csr
+JEFF_DICT = {
+    'contactKey': 'Bezos.Jeff', 'homeAddress': '', 'businessNumber': '1800 3000 9009',
+    'homeNumber': '', 'faxNumber': '', 'Notes': '', 'Nickname': '', 'Extension': '',
+    'Title': 'CEO of Amazon', 'mobileNumber': '', 'pagerNumber': '', 'Birthday': '',
+    'emailHome': '', 'firstName': 'Jeff', 'MI': '', 'Salutation': 'None', 'City': '',
+    'stateProvince': '', 'zipPostal': '', 'properName': 'Mr. Jeff Bezos', 'spouseName': '',
+    'lastName': 'Bezos', 'emailBusiness': '', 'doNotSolicit': 'FALSE', 'busCity': 'Seattle',
+    'busCountry': 'USA', 'busState': 'WA', 'busStreet': '410 Terry Ave N', 'busZip': '98109',
+    'Account': 'Amazon', 'otherTelephone': '', 'mainTelephone': '',
+    'addModifyUser': 'Simpson.Barry.T', 'addModifyDate': '20190815', 'mailCode': '',
+    'nextContact': '', 'lastContact': '20190807', 'DOB': '', 'Relates to Activity': '',
+    'Relates to Account': 'Amazon', 'Relates to Opportunity': '', 'LinkedInLink': '',
+    'Relates to mailingType': '', 'Relates to Attachment': '',
+    'twitterLink': 'https://twitter.com/jeffbezos', 'Relates to Address': 'Customer service PO Box',
+    'Relates to History': '', 'Relates to Expense': '',
+    'FacebookLink': 'https://www.facebook.com/groups/1654560838132448/',
+    'Relates to contactType': '', 'Relates to contactInterest': '',
+    'Relates to Employee': 'Simpson.Barry.T', 'Handheld Device Employee': 'Simpson.Barry.T',
+    'ID': '27BH1B', 'cityStateZip': 'Seattle, WA 98109', 'Influence': 'Low',
+    'Relates to detailNote': '', 'Manager of Contact': '', 'Managed by Contact': '',
+    'isPrimary': 'FALSE', 'Relates to Salutation': 'Mr.'
+}
+GEOFF_DICT = {
+    'contactKey': 'Bezos.Geoff', 'homeAddress': 'GEOFF', 'businessNumber': '1800 3000 9009',
+    'homeNumber': '', 'faxNumber': '', 'Notes': 'GEOFF', 'Nickname': '', 'Extension': '',
+    'Title': 'CEO of Amazon', 'mobileNumber': '', 'pagerNumber': '', 'Birthday': '',
+    'emailHome': '', 'firstName': 'Geoff', 'MI': '', 'Salutation': 'None', 'City': '',
+    'stateProvince': '', 'zipPostal': '', 'properName': 'Mr. Jeff Bezos', 'spouseName': '',
+    'lastName': 'Bezos', 'emailBusiness': '', 'doNotSolicit': 'FALSE', 'busCity': 'Seattle',
+    'busCountry': 'USA', 'busState': 'WA', 'busStreet': '410 Terry Ave N', 'busZip': '98109',
+    'Account': 'Amazon', 'otherTelephone': '', 'mainTelephone': '',
+    'addModifyUser': 'Simpson.Barry.T', 'addModifyDate': '20190815', 'mailCode': '',
+    'nextContact': '', 'lastContact': '20190807', 'DOB': '', 'Relates to Activity': '',
+    'Relates to Account': 'Amazon', 'Relates to Opportunity': '', 'LinkedInLink': '',
+    'Relates to mailingType': '', 'Relates to Attachment': '',
+    'twitterLink': 'https://twitter.com/jeffbezos', 'Relates to Address': 'Customer service PO Box',
+    'Relates to History': '', 'Relates to Expense': '',
+    'FacebookLink': 'https://www.facebook.com/groups/1654560838132448/',
+    'Relates to contactType': '', 'Relates to contactInterest': '',
+    'Relates to Employee': 'Simpson.Barry.T', 'Handheld Device Employee': 'Simpson.Barry.T',
+    'ID': '27BH1B', 'cityStateZip': 'Seattle, WA 98109', 'Influence': 'Low',
+    'Relates to detailNote': '', 'Manager of Contact': '', 'Managed by Contact': '',
+    'isPrimary': 'FALSE', 'Relates to Salutation': 'Mr.'
+}
+GEOFF_KEY = GEOFF_DICT.get('contactKey')
+JEFF_KEY = JEFF_DICT.get('contactKey')
 
-
-@pytest.fixture
-def contact_csr():
-    with api.csr_context("Contact") as csr:
-        yield csr
-
-
-@pytest.fixture
-def sale_csr():
-    with api.csr_context("Sale") as csr:
-        yield csr
-
-
-@pytest.fixture(scope="session")
-def random_hire_record():
-    rec = random.choice(sample_data.hires)
-    return rec
+UPDATE_PKG_1 = {'firstName': 'test2'}
+PK_VAL = 'Col0 Val'
+__all__ = ['contact_csr', 'JEFF_DICT', 'GEOFF_DICT', 'GEOFF_KEY', 'JEFF_KEY', 'UPDATE_PKG_1']
--- tests/test_tutorial_db.py
+++ tests/test_tutorial_db.py
@@ -1,7 +1,15 @@
-import pycommence.api
-from pycommence.api import Csr, csr_context
+import pytest
 
-"""USE TUTORIAL DB"""
+from pycommence.api import Csr, csr_context
+from .conftest import GEOFF_DICT, GEOFF_KEY, JEFF_KEY, UPDATE_PKG_1
+
+
+@pytest.fixture
+def contact_csr():
+    with csr_context("Contact") as csr:
+        if not csr.db_name == 'Tutorial':
+            raise ValueError("Expected Tutorial DB")
+        yield csr
 
 
 def test_contact_csr(contact_csr):
@@ -16,25 +24,35 @@ def test_get_records(contact_csr: Csr):
 
 def test_clear_filter(contact_csr: Csr):
     row_count1 = contact_csr.row_count
-    contact_csr.filter_by_pk('Bezos.Jeff')
+    contact_csr.filter_by_pk(JEFF_KEY)
     row_count2 = contact_csr.row_count
     assert row_count2 == 1
     contact_csr.clear_filter()
     assert contact_csr.row_count == row_count1
 
+
 def test_add_record(contact_csr: Csr):
+    contact_csr.delete_record(pk_val=GEOFF_KEY, empty='ignore')
     row_count1 = contact_csr.row_count
-    contact_csr.add_record(pk_val='Col0 Val', package={'firstName': 'test2'})
+    contact_csr.add_record(pk_val=GEOFF_KEY, package=GEOFF_DICT)
     row_count2 = contact_csr.row_count
     assert row_count2 == row_count1 + 1
 
-    with pycommence.api.csr_context('Contact') as cs2:
-        res = cs2.records_by_field(field_name='firstName', value='test2')
-    assert res
-    assert res[0]['firstName'] == 'test2'
+    with csr_context('Contact') as cs2:
+        res = cs2.one_record(GEOFF_KEY)
+    assert len(res) == 61  # 61 fields in Contact
+    for k, v in GEOFF_DICT.items():
+        assert res[k] == v
 
 
-def test_tst():
-    with csr_context("cat") as csr:
-        recs = csr.records()
-        ...
+def test_edit_record(contact_csr: Csr):
+    contact_csr.edit_record(pk_val=GEOFF_KEY, package=UPDATE_PKG_1)
+    res = contact_csr.one_record(pk_val=GEOFF_KEY)
+    for k, v in UPDATE_PKG_1.items():
+        assert res[k] == v
+
+
+def test_temp_filter(contact_csr: Csr):
+    with contact_csr.temporary_filter_pk(JEFF_KEY):
+        assert contact_csr.row_count == 1
+    assert contact_csr.row_count > 1
