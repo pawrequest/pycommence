1384a99014ed0bbdef79f024f0a20c64d100a10f
paw <pawrequest@users.noreply.github.com>
2024-04-14 12:13:16 +0100

    Revert "pycommenceobj, maybe doc build issues bc same lc name as package?"
    This reverts commit 4149f84c6ca49b87d220fac8ecd81c8eca559aa8.

--- tests/test_pycommence.py
+++ tests/test_pycommence.py
@@ -5,7 +5,7 @@ import pytest
 from pycommence.cursor import get_csr
 from .conftest import GEOFF_DICT, GEOFF_KEY, JEFF_KEY, UPDATE_PKG_1
 from pycommence.pycmc_types import CmcError
-from pycommence import PyCommenceObj
+from pycommence import PyCommence
 
 
 @pytest.fixture
@@ -15,15 +15,15 @@ def pycmc():
     csr = get_csr("Contact")
     if not csr.db_name == 'Tutorial':
         raise ValueError("Expected Tutorial DB")
-    yield PyCommenceObj(csr=csr)
+    yield PyCommence(csr=csr)
 
 
 def test_pycmc(pycmc):
-    assert isinstance(pycmc, PyCommenceObj)
+    assert isinstance(pycmc, PyCommence)
 
 
 @contextlib.contextmanager
-def temp_geoff(pycmc: PyCommenceObj):
+def temp_geoff(pycmc: PyCommence):
     """Temporarily add GEOFF_DICT to Contact table. Remove after use."""
     try:
         pycmc.add_record(pk_val=GEOFF_KEY, package=GEOFF_DICT)
@@ -43,26 +43,26 @@ def test_temp_geoff(pycmc):
         pycmc.one_record(GEOFF_KEY)
 
 
-def test_get_records(pycmc: PyCommenceObj):
+def test_get_records(pycmc: PyCommence):
     res = pycmc.records()
     assert isinstance(res, list)
     assert isinstance(res[0], dict)
 
 
-def test_get_one_record(pycmc: PyCommenceObj):
+def test_get_one_record(pycmc: PyCommence):
     res = pycmc.one_record(JEFF_KEY)
     assert isinstance(res, dict)
     assert res['firstName'] == 'Jeff'
 
 
-def test_get_records_by_field(pycmc: PyCommenceObj):
+def test_get_records_by_field(pycmc: PyCommence):
     res = pycmc.records_by_field('firstName', 'Jeff')
     assert isinstance(res, list)
     assert isinstance(res[0], dict)
     assert res[0]['firstName'] == 'Jeff'
 
 
-def test_edit_record(pycmc: PyCommenceObj):
+def test_edit_record(pycmc: PyCommence):
     with temp_geoff(pycmc):
         original = pycmc.one_record(GEOFF_KEY)
 
@@ -76,13 +76,13 @@ def test_edit_record(pycmc: PyCommenceObj):
         assert reverted == original
 
 
-def test_add_record(pycmc: PyCommenceObj):
+def test_add_record(pycmc: PyCommence):
     row_count1 = pycmc.csr.row_count
     with temp_geoff(pycmc):
         row_count2 = pycmc.csr.row_count
         assert row_count2 == row_count1 + 1
 
-        handler2 = PyCommenceObj.from_table_name('Contact')
+        handler2 = PyCommence.from_table_name('Contact')
         res = handler2.one_record(GEOFF_KEY)
         assert len(res) == 61  # 61 fields in Contact
         for k, v in GEOFF_DICT.items():
@@ -92,13 +92,13 @@ def test_add_record(pycmc: PyCommenceObj):
     assert row_count3 == row_count1
 
 
-def test_add_duplicate_raises(pycmc: PyCommenceObj):
+def test_add_duplicate_raises(pycmc: PyCommence):
     with temp_geoff(pycmc):
         with pytest.raises(CmcError):
             pycmc.add_record(pk_val=GEOFF_KEY, package=GEOFF_DICT)
 
 
-def test_add_replace(pycmc: PyCommenceObj):
+def test_add_replace(pycmc: PyCommence):
     with temp_geoff(pycmc):
         pycmc.add_record(pk_val=GEOFF_KEY, package=UPDATE_PKG_1, existing='replace')
         res = pycmc.one_record(GEOFF_KEY)
@@ -106,7 +106,7 @@ def test_add_replace(pycmc: PyCommenceObj):
             assert res[k] == v
 
 
-def test_add_update(pycmc: PyCommenceObj):
+def test_add_update(pycmc: PyCommence):
     with temp_geoff(pycmc):
         pycmc.add_record(pk_val=GEOFF_KEY, package=UPDATE_PKG_1, existing='update')
         res = pycmc.one_record(GEOFF_KEY)
